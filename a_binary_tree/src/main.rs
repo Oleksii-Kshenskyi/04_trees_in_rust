2fbc63142cf0d83a4e74364c3f93512a874aeb65
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2021-07-24 17:15:37 +0300

    Binary Tree: finished implementation:
    - The very first assert test (testing that insert worked correctly) fails. Debug time.

--- a_binary_tree/src/main.rs
+++ a_binary_tree/src/main.rs
@@ -1,10 +1,40 @@
+use tree::BinaryTree;
+
 mod tree;
 
-// Binary tree should imlement these operations:
+// Binary tree should implement these operations:
 // - Find element;
 // - Insert element;
 // - Remove element.
+fn s(s: &str) -> String {
+    s.to_string()
+}
 
 fn main() {
-    println!("Hello, world!");
+    let mut tree = BinaryTree::<String, String>::new();
+
+    tree.insert(s("One"), s("1"));
+    assert_eq!(Some(s("1")), tree.find(&s("One")));
+    tree.insert(s("Two"), s("2"));
+    assert_eq!(Some(s("2")), tree.find(&s("Two")));
+    tree.insert(s("3"), s("Three"));
+    assert_eq!(Some(s("3")), tree.find(&s("Three")));
+    assert_eq!(None, tree.find(&s("KEKW")));
+
+    assert_eq!(Ok(()), tree.delete(&s("Three")));
+    assert_eq!(Err(()), tree.delete(&s("OMEGA")));
+    assert_eq!(None, tree.find(&s("Three")));
+    assert_eq!(Some(s("2")), tree.find(&s("Two")));
+    assert_eq!(Some(s("1")), tree.find(&s("One")));
+
+    assert_eq!(Ok(()), tree.delete(&s("Two")));
+    assert_eq!(None, tree.find(&s("Three")));
+    assert_eq!(None, tree.find(&s("Two")));
+    assert_eq!(Some(s("1")), tree.find(&s("One")));
+
+    assert_eq!(Ok(()), tree.delete(&s("One")));
+    assert_eq!(None, tree.find(&s("Three")));
+    assert_eq!(None, tree.find(&s("Two")));
+    assert_eq!(None, tree.find(&s("One")));
+    assert_eq!(true, tree.is_empty());
 }
--- a_binary_tree/src/tree.rs
+++ a_binary_tree/src/tree.rs
@@ -3,6 +3,20 @@ use std::hash::Hash;
 use std::boxed::Box;
 use std::cmp::Ordering;
 
+enum DeletionType {
+    LeafNode,
+    HasLeftChild,
+    HasRightChild,
+    HasBothChildren,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
+enum NodeStatus {
+    IsLeftChild,
+    IsRoot,
+    IsRightChild,
+}
+
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
 pub struct BinaryTree<K, V> 
 where K: Eq + Ord + Hash + Clone,
@@ -17,9 +31,34 @@ where
     V: Clone {
     key: K,
     value: V,
+    status: NodeStatus,
     right_child: Option<Box<Node<K, V>>>,
     left_child: Option<Box<Node<K, V>>>,
 }
+
+impl<K, V> Node<K, V>
+where 
+    K: Eq + Ord + Hash + Clone,
+    V: Clone {
+        fn determine_deletion_type(&self) -> DeletionType {
+            let childrenvec: Vec<Option<Box<Node<K, V>>>> = vec![self.left_child.clone(), self.right_child.clone()];
+            if childrenvec.iter().all(|x| { x.is_none() }) {
+                DeletionType::LeafNode
+               }
+            else if childrenvec.iter().all(|x| { x.is_some() }) {
+                DeletionType::HasBothChildren
+            }
+            else if self.left_child.is_some() && self.right_child.is_none() {
+                DeletionType::HasLeftChild
+            }
+            else if self.right_child.is_some() && self.left_child.is_none() {
+                DeletionType::HasRightChild
+            }
+            else {
+                unreachable!("BinaryTree::determine_deletion_type(): mistake in the algorithm?")
+            }
+        }
+    }
 
 impl<K, V> BinaryTree<K, V> 
 where 
@@ -29,6 +68,10 @@ where
         Self {
             root: None
         }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.root.is_none()
     }
 
     pub fn find(&self, key: &K) -> Option<V> {
@@ -54,11 +97,11 @@ where
     }
 
     pub fn insert(&mut self, key: K, value: V) {
-        self.insert_internal(&mut self.root.clone(), key, value);
+        self.insert_internal(&mut self.root.clone(), &mut NodeStatus::IsRoot, key, value);
     }
-    fn insert_internal(&mut self, root: &mut Option<Box<Node<K, V>>>, key: K, value: V) {
+    fn insert_internal(&mut self, root: &mut Option<Box<Node<K, V>>>, status: &mut NodeStatus, key: K, value: V) {
         if root.is_none() {
-            BinaryTree::create_node_at(root, key, value);
+            BinaryTree::create_node_at(root, status.clone(), key, value);
             return;
         }
 
@@ -66,29 +109,184 @@ where
         let mutnode = boxclone.as_mut();
         match mutnode.key.cmp(&key) {
             Ordering::Less => {
-                self.insert_internal(&mut mutnode.left_child, key, value);
+                self.insert_internal(&mut mutnode.left_child, &mut NodeStatus::IsLeftChild, key, value);
             },
             Ordering::Equal => {
-                BinaryTree::create_node_at(root, key, value);
+                BinaryTree::create_node_at(root, status.clone(), key, value);
             },
             Ordering::Greater => {
-                self.insert_internal(&mut mutnode.right_child, key, value);
+                self.insert_internal(&mut mutnode.right_child, &mut NodeStatus::IsRightChild, key, value);
             }
         };
     }
 
-    pub fn remove(&mut self, key: &K) -> Result<(), ()> {
-        let _ = key;
-        Ok(())
+    pub fn delete(&mut self, key: &K) -> Result<(), ()> {
+        self.delete_internal(&mut self.root.clone(), &mut None, key)
+    }
+    fn delete_internal(&mut self, root: &mut Option<Box<Node<K, V>>>, parent: &mut Option<Box<Node<K, V>>>, key: &K) -> Result<(), ()> {
+        if root.is_none() {
+            return Err(());
+        }
+
+        let mut boxclone = root.clone().unwrap();
+        let mutnode = boxclone.as_mut();
+        match mutnode.key.cmp(&key) {
+            Ordering::Less => {
+                return self.delete_internal(&mut mutnode.left_child, &mut self.root.clone(), key);
+            },
+            Ordering::Equal => {
+                self.perform_deletion(&mut root.clone(), parent);
+                return Ok(());
+            },
+            Ordering::Greater => {
+                return self.delete_internal(&mut mutnode.right_child, &mut self.root.clone(), key);
+            }
+        }
+    }
+    fn perform_deletion(&mut self, delete_me: &mut Option<Box<Node<K, V>>>, parent: &mut Option<Box<Node<K, V>>>) {
+        let delete_me_node = *delete_me.clone().unwrap();
+        match delete_me_node.determine_deletion_type() {
+            DeletionType::LeafNode => {
+                match delete_me_node.status {
+                    NodeStatus::IsLeftChild => {
+                        let mut parent_box = parent.clone().unwrap();
+                        let parent_node = parent_box.as_mut();
+
+                        parent_node.left_child = None;
+
+                        *delete_me = None;
+                    },
+                    NodeStatus::IsRoot => {
+                        *delete_me = None;
+                    },
+                    NodeStatus::IsRightChild => {
+                        let mut parent_box = parent.clone().unwrap();
+                        let parent_node = parent_box.as_mut();
+
+                        parent_node.right_child = None;
+
+                        *delete_me = None;
+                    },
+                }
+            },
+            DeletionType::HasLeftChild => {
+                match delete_me_node.status {
+                    NodeStatus::IsLeftChild => {
+                        let mut parent_box = parent.clone().unwrap();
+                        let parent_node = parent_box.as_mut();
+                        let delete_me_left_child = delete_me.clone().unwrap().left_child;
+
+                        parent_node.left_child = delete_me_left_child;
+
+                        *delete_me = None;
+                    },
+                    NodeStatus::IsRoot => {
+                        let delete_me_left_child = delete_me.clone().unwrap().left_child;
+
+                        self.root = delete_me_left_child;
+
+                        *delete_me = None;
+                    },
+                    NodeStatus::IsRightChild => {
+                        let mut parent_box = parent.clone().unwrap();
+                        let parent_node = parent_box.as_mut();
+                        let delete_me_left_child = delete_me.clone().unwrap().left_child;
+
+                        parent_node.right_child = delete_me_left_child;
+
+                        *delete_me = None;
+                    },
+                }
+            },
+            DeletionType::HasRightChild => {
+                match delete_me_node.status {
+                    NodeStatus::IsLeftChild => {
+                        let mut parent_box = parent.clone().unwrap();
+                        let parent_node = parent_box.as_mut();
+                        let delete_me_right_child = delete_me.clone().unwrap().right_child;
+
+                        parent_node.left_child = delete_me_right_child;
+
+                        *delete_me = None;
+                    },
+                    NodeStatus::IsRoot => {
+                        let delete_me_right_child = delete_me.clone().unwrap().right_child;
+
+                        self.root = delete_me_right_child;
+
+                        *delete_me = None;
+                    },
+                    NodeStatus::IsRightChild => {
+                        let mut parent_box = parent.clone().unwrap();
+                        let parent_node = parent_box.as_mut();
+                        let delete_me_right_child = delete_me.clone().unwrap().right_child;
+
+                        parent_node.right_child = delete_me_right_child;
+
+                        *delete_me = None;
+                    },
+                }
+            },
+            DeletionType::HasBothChildren => {
+                match delete_me_node.status {
+                    NodeStatus::IsLeftChild => {
+                        let mut parent_box = parent.clone().unwrap();
+                        let parent_node = parent_box.as_mut();
+                        let delete_me_left_child = delete_me.clone().unwrap().left_child;
+                        let delete_me_right_child = delete_me.clone().unwrap().right_child;
+
+                        parent_node.left_child = delete_me_left_child.clone();
+
+                        let mut dmlc_clone = delete_me_left_child.clone();
+                        let dmlc_node = dmlc_clone.as_mut().unwrap().as_mut();
+
+                        dmlc_node.right_child = delete_me_right_child;
+
+                        *delete_me = None;
+                    },
+                    NodeStatus::IsRoot => {
+                        let delete_me_box = delete_me.clone().unwrap();
+                        let delete_me_node = delete_me_box.as_ref();
+                        let delete_me_left_child = delete_me_node.left_child.clone();
+                        let delete_me_right_child = delete_me_node.right_child.clone();
+
+                        self.root = delete_me_left_child.clone();
+
+                        let mut dmlc_clone = delete_me_left_child.clone();
+                        let dmlc_node = dmlc_clone.as_mut().unwrap().as_mut();
+
+                        dmlc_node.right_child = delete_me_right_child;
+
+                        *delete_me = None;
+                    },
+                    NodeStatus::IsRightChild => {
+                        let mut parent_box = parent.clone().unwrap();
+                        let parent_node = parent_box.as_mut();
+                        let delete_me_right_child = delete_me.clone().unwrap().right_child;
+                        let delete_me_left_child = delete_me.clone().unwrap().left_child;
+
+                        parent_node.right_child = delete_me_right_child.clone();
+
+                        let mut dmrc_clone = delete_me_right_child.clone();
+                        let dmrc_node = dmrc_clone.as_mut().unwrap().as_mut();
+
+                        dmrc_node.left_child = delete_me_left_child;
+
+                        *delete_me = None;
+                    },
+                }
+            },
+        }
     }
 
 
-    fn create_node_at(at_root: &mut Option<Box<Node<K, V>>>, key: K, value: V) {
+    fn create_node_at(at_root: &mut Option<Box<Node<K, V>>>, status: NodeStatus, key: K, value: V) {
         *at_root = Some(
             Box::new(
                 Node {
                     key,
                     value,
+                    status,
                     left_child: None,
                     right_child: None,
                 }
